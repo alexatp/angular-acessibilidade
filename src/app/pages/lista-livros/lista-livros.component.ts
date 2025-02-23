7960e403685c501ad764e121c37839b58ea080a6
Alexandre Teixeira <alextp@gmail.com>
2025-02-23 01:19:18 -0300

    Implementando mensagens de feedback

--- src/app/pages/lista-livros/lista-livros.component.ts
+++ src/app/pages/lista-livros/lista-livros.component.ts
@@ -8,6 +8,7 @@ import { LivrosResultado, Item } from '../../models/interfaces';
 import { LivroVolumeInfo } from '../../models/livroVolumeInfo';
 import { LivroService } from '../../service/livro.service';
 import { LivroComponent } from '../../componentes/livro/livro.component';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 
 const PAUSA = 300;
@@ -31,7 +32,9 @@ export class ListaLivrosComponent implements AfterViewInit {
 
   @ViewChild('campoBuscaElement') campoBuscaElement!: ElementRef;
 
-  constructor(private service: LivroService) { }
+  constructor(
+    private service: LivroService,
+    private liveAnnouncer: LiveAnnouncer) { }
 
   ngAfterViewInit() {
     this.campoBuscaElement.nativeElement.focus()
@@ -50,6 +53,7 @@ export class ListaLivrosComponent implements AfterViewInit {
     }),
     tap((resultado) => {
       this.livrosResultado = resultado;
+      this.liveAnnouncer.announce(`${this.livrosResultado.totalItems} resultados encontrados`)
     }),
     map((resultado) => resultado.items ?? []),
     map((items) => this.livrosResultadoParaLivros(items)),
